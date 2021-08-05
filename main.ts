100eff9ab914235b584006c4cf81c6f4f6849f9c
Michael Hanson <186724+mybuddymichael@users.noreply.github.com>
2021-08-04 20:31:11 -0700

    Make widths configurable

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface HideSidebarsWhenNarrowSettings {
+  leftMinWidth: number;
+  rightMinWidth: number;
 }
 
 const DEFAULT_SETTINGS: HideSidebarsWhenNarrowSettings = {
-}
+  leftMinWidth: 1400,
+  rightMinWidth: 1100,
+};
 
 export default class HideSidebarsWhenNarrowPlugin extends Plugin {
   settings: HideSidebarsWhenNarrowSettings;
@@ -13,36 +17,33 @@ export default class HideSidebarsWhenNarrowPlugin extends Plugin {
     console.log('loading HideSideBarsWhenNarrowPlugin');
     this.app.workspace.onLayoutReady(() => {
       this.loadSettings().then(() => {
-        // this.addSettingTab(new SampleSettingTab(this.app, this));
+        this.addSettingTab(new SettingsTab(this.app, this));
 
         this.toggleSidebars();
 
         this.registerDomEvent(window, 'resize', (_) => {
-          window.setTimeout(this.toggleSidebars, 100)
+          window.setTimeout(this.toggleSidebars.bind(this), 100);
         });
       });
-    })
-  }
-
-  onunload() {
-    console.log('unloading plugin');
+    });
   }
 
   toggleSidebars() {
+    console.log(this.settings);
     const width = window.outerWidth;
-    // TODO: make these widths configurable
-    if (width < 1400 && width >= 1001) {
-      // collapse left only
-      !this.app.workspace.leftSplit.collapsed && this.app.workspace.leftSplit.collapse();
-      this.app.workspace.rightSplit.collapsed && this.app.workspace.rightSplit.expand();
-    } else if (width < 1000) {
-      // collapse both
-      !this.app.workspace.rightSplit.collapsed && this.app.workspace.rightSplit.collapse();
-      !this.app.workspace.leftSplit.collapsed && this.app.workspace.leftSplit.collapse();
+    if (width < this.settings.leftMinWidth) {
+      !this.app.workspace.leftSplit.collapsed &&
+        this.app.workspace.leftSplit.collapse();
     } else {
-      // expand all
-      this.app.workspace.leftSplit.collapsed && this.app.workspace.leftSplit.expand();
-      this.app.workspace.rightSplit.collapsed && this.app.workspace.rightSplit.expand();
+      this.app.workspace.leftSplit.collapsed &&
+        this.app.workspace.leftSplit.expand();
+    }
+    if (width < this.settings.rightMinWidth) {
+      !this.app.workspace.rightSplit.collapsed &&
+        this.app.workspace.rightSplit.collapse();
+    } else {
+      this.app.workspace.rightSplit.collapsed &&
+        this.app.workspace.rightSplit.expand();
     }
   }
 
@@ -52,6 +53,7 @@ export default class HideSidebarsWhenNarrowPlugin extends Plugin {
 
   async saveSettings() {
     await this.saveData(this.settings);
+    this.toggleSidebars();
   }
 }
 
@@ -68,18 +70,43 @@ class SettingsTab extends PluginSettingTab {
 
     containerEl.empty();
 
-    containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
+    containerEl.createEl('h3', { text: 'Hide Sidebars When Narrow' });
 
     new Setting(containerEl)
-      .setName('Setting #1')
-      .setDesc('It\'s a secret')
-      .addText(text => text
-        .setPlaceholder('Enter your secret')
-        .setValue('')
-        .onChange(async (value) => {
-          console.log('Secret: ' + value);
-          // this.plugin.settings.mySetting = value;
-          await this.plugin.saveSettings();
-        }));
+      .setName('Minimum window width for the left sidebar')
+      .setDesc('How wide should the window be for the left sidebar to show?')
+      .addText((text) =>
+        text
+          .setPlaceholder(`Default: ${DEFAULT_SETTINGS.leftMinWidth}`)
+          .setValue(this.plugin.settings.leftMinWidth.toString())
+          .onChange(async (value) => {
+            const num = parseInt(value);
+            if (num) {
+              this.plugin.settings.leftMinWidth = num;
+            } else {
+              this.plugin.settings.leftMinWidth = DEFAULT_SETTINGS.leftMinWidth;
+            }
+            await this.plugin.saveSettings();
+          })
+      );
+
+    new Setting(containerEl)
+      .setName('Minimum window width for the right sidebar')
+      .setDesc('How wide should the window be for the right sidebar to show?')
+      .addText((text) =>
+        text
+          .setPlaceholder(`Default: ${DEFAULT_SETTINGS.rightMinWidth}`)
+          .setValue(this.plugin.settings.rightMinWidth.toString())
+          .onChange(async (value) => {
+            const num = parseInt(value);
+            if (num) {
+              this.plugin.settings.rightMinWidth = num;
+            } else {
+              this.plugin.settings.rightMinWidth =
+                DEFAULT_SETTINGS.rightMinWidth;
+            }
+            await this.plugin.saveSettings();
+          })
+      );
   }
 }
